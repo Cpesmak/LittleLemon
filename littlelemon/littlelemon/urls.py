dc4f92d0960457e72e5b29a877264828377088ab
Cpesmak <cpesmak@gmail.com>
2025-01-31 14:22:38 +0000

    Sixth commit

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -1,9 +1,16 @@
-
+from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
+from restaurant.views import BookingViewset
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewset)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('restaurant/', include('restaurant.urls')),
    path('restaurant/menu/',include('restaurant.urls')),
+   path('restaurant/booking/',include(router.urls)),
+   path('auth/', include('djoser.urls')),
+   path('auth/', include('djoser.urls.authtoken'))
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from .models import Menu
-from .serializers import MenuSerializer
-from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
-class MenuItemView(generics.ListCreateAPIView):
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+class BookingViewset(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
